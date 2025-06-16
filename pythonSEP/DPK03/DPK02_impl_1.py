1e8107aa7802c2a62e59e1eb8db3db7f774b6acd
Luis Fernando Neu <luisneu@gmail.com>
2025-06-16 07:34:41 -0300

    03 - 01

---
+++ pythonSEP/DPK03/DPK02_impl_1.py
@@ -0,0 +1,30 @@
+class LookupService:
+    def __init__(self):
+        self.id_to_name = {}
+        self.name_to_email = {}
+        self.email_to_name = {}
+    
+    def add_entry(self, id_, name, email):
+        self.id_to_name[id_] = name
+        self.name_to_email[name] = email
+        self.email_to_name[email] = name
+
+    def lookup(self, key):
+        if isinstance(key, int):
+            return self.id_to_name.get(key, "Not found")
+        elif isinstance(key, str):
+            if key in self.name_to_email:
+                return self.name_to_email[key]
+            elif key in self.email_to_name:
+                return self.email_to_name[key]
+            else:
+                return "Not found"
+        else:
+            return "Invalid key type"
+if __name__ == "__main__":
+    service = LookupService()
+    service.add_entry(1, "John", "john@john.jhon.com")
+
+    print(service.lookup(1))
+    print(service.lookup("John"))
+    print(service.lookup("john@john.jhon.com"))
